0cf574fca0100f1fc5d811a4b897660d184be556
krzeminski <49924680+krzeminski@users.noreply.github.com>
2020-01-23 00:27:37 +0100

    fock

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const db = require('./queries')
-const { Pool, Client }  = require('pg');
 
 const app = express();
 
@@ -13,44 +12,46 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const pool = new Pool({
-  user: 'adrian',
-  host: 'localhost',
-  database: 'wiadro',
-  password: 'admin',
-  port: 5432,
+
+
+// let sourceCities = [];
+let airway = [];
+
+
+
+app.get("/", async function(req,res){
+  const sourceCities = await db.getSourceCities();
+  console.log(sourceCities);
+
+  sourceCities.forEach(element =>{
+     airway.push({
+       src: element,
+       dest: db.getDestCities(element)
+     });
+   });
+
+  res.render("home", {airway: airway});
 });
 
-const client = new Client({
- user: 'postgres',
- host: 'localhost',
- database: 'wiadro',
- password: 'admin',
- port: 5432,
-});
-client.connect()
-
-
-let searchedFlights = [];
-
-app.get("/", function(req,res){
-
-  //creme de la creme
-  client.query('SELECT DISTINCT source_city FROM flight ORDER BY source_city ASC', (error, results) => {
-    if (error) {
-      console.log(error);
-    }else{
-      console.log(results.rows);
-    }
-    client.end();
-  });
-
-
-  res.render("home");
-});
 
 app.get("/flights", function(req,res){
   res.render("flights");
+});
+
+app.post("/flights", function(req,res){
+  let flight = {
+    origin:req.body.origin,
+    destination:req.body.destination,
+    departure:req.body.departure,
+    return:req.body.return,
+    passengers:req.body.passengers
+  };
+
+  console.log(flight);
+
+  db.getFlights(flight);
+
+  res.redirect("/flights");
 });
 
 app.get("/sign-in", function(req,res){
@@ -61,27 +62,30 @@ app.get("/sign-up", function(req,res){
   res.render("sign-up");
 });
 
-//Te wywołują funkcje z queries pod localhost:3000/users...
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.post("/sign-up", function(req,res){
+  let account = {
+    name: req.body.name,
+    last_name: req.body.surname,
+    password: req.body.password,
+    email: req.body.email,
+    phone: req.body.phone
+  }
 
+  db.createAccount(account);
 
-app.post("/", function(req,res){
-  let flight = {
-    origin:req.body.origin,
-    destination:req.body.destination,
-    departure:req.body.departure,
-    return:req.body.return,
-    passengers:req.body.passengers
-  };
-  searchedFlights.push(flight);
-  res.redirect("/");
+  res.render("sign-in");
 });
+
+
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+// //Te wywołują funkcje z queries pod localhost:3000/users...
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
-const Pool = require('pg').Pool;
+const { Pool, Client }  = require('pg');
+
 const pool = new Pool({
   user: 'adrian',
   host: 'localhost',
@@ -7,89 +8,174 @@ const pool = new Pool({
   port: 5432,
 });
 
+const client = new Client({
+ user: 'postgres',
+ host: 'localhost',
+ database: 'wiadro',
+ password: 'admin',
+ port: 5432,
+});
+client.connect()
 
-const getSourceCities = (request, response) => {
-  pool.query('SELECT source_city FROM flight ORDER BY source_city ASC;', (error, results) => {
-    if (error) {
-      throw error
+function getSourceCities(){
+  //creme de la creme
+  client.query('SELECT DISTINCT source_city FROM flight ORDER BY source_city ASC', (err, res) => {
+    if (err) {
+      console.log("Błąd w getSourceCities", err);
+    }else{
+      // console.log(res.rows);
+      return res.rows;
     }
-    response.status(200).json(results.rows)
+    client.end();
+  });
+};
+
+function getDestCities(){
+  client.query('SELECT dest_city FROM flight ORDER BY dest_city ASC;', (err, res) => {
+    if (err) {
+      console.log("Błąd w getDestCities",err);
+    }else{
+      console.log(res.rows);
+      return res.rows;
+    }
+    //response.status(200).json(results.rows);
+    client.end();
   })
 };
 
-const getDestCities = (request, response) => {
-  pool.query('SELECT dest_city FROM flight ORDER BY dest_city ASC;', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-};
+function getAirway(){
 
-
-
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-};
-
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-};
-
-const createUser = (request, response) => {
-  const { name, email } = request.body
-
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${result.insertId}`)
-  })
 }
 
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
-
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
+function getFlights(flight){
+  let foundFlight;
+  client.query('SELECT a.name, b.model, f.source_city, f.dest_city, f.flight_date, f.departure_time, f.arrival_time FROM airline a, aircraft b, flight f WHERE f.source_city=$1 and f.dest_city=$2 and f.flight_date=$3 and f.airline_id=a.airline_id and f.aircraft_id=b.aircraft_id;',
+   [flight.origin, flight.destination, flight.departure], (err, res) => {
+    if (err) {
+      console.log("Błąd w getFlights", err);
+    }else{
+      console.log(res.rows);
+      foundFlight = res.rows;
     }
-  )
-}
+    //response.status(200).json(results.rows);
+    client.end();
+  });
+};
 
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
+
+function createAccount(account){
+  client.query('INSERT INTO passenger (first_name, last_name, password, email, phone) VALUES ($1, $2, $3, $4, $5);',
+   [account.name, account.last_name, account.password, account.email, account.phone], (err,res) => {
+  	if (err) {
+      console.log("Błąd w createAccount", err);
+    }else{
+      console.log('Account created!');
     }
-    response.status(200).send(`User deleted with ID: ${id}`)
+    client.end();
   })
-}
+};
+
+
+
 
 module.exports = {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
+  getSourceCities,
+  getDestCities,
+  getFlights,
+  createAccount,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------
+
+
+// const getUsers = (request, response) => {
+//   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// };
+//
+// const getUserById = (request, response) => {
+//   const id = parseInt(request.params.id)
+//
+//   pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// };
+//
+// const createUser = (request, response) => {
+//   const { name, email } = request.body
+//
+//   pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(201).send(`User added with ID: ${result.insertId}`)
+//   })
+// }
+//
+// const updateUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//   const { name, email } = request.body
+//
+//   pool.query(
+//     'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+//     [name, email, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).send(`User modified with ID: ${id}`)
+//     }
+//   )
+// }
+//
+// const deleteUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//
+//   pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).send(`User deleted with ID: ${id}`)
+//   })
+// }
+//
+// module.exports = {
+//   getSourceCities,
+//   getDestCities,
+//   getUsers,
+//   getUserById,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+// }
